9eef9411bf3bcf9de9352b1e956ff687100e0834
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-05-04 15:24:03 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
+"use client";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+
+import { motion } from 'framer-motion';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { AudioWaveform, Bot } from "lucide-react";
 
 export default function Home() {
+  const [isRecording, setIsRecording] = useState(false);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="flex pt-10 w-full justify-center">
+        <button className="relative block rounded-full border-8 border-amber-50" onClick={() => {
+          
+        }}>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <motion.div
+            className="absolute top-0 left-0 w-full h-full bg-blue-500 rounded-full opacity-30"
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.1, ease: 'easeOut' }}
+          />
+          <Image
+            className={twMerge("dark:invert","rounded-full")}
+            src="/mic.jpg"
+            alt="Microphone logo"
+            width={300}
+            height={180}
+            priority
+          />
+        </button>
+      </div>
+
+      <div className="text-2xl font-bold text-gray-500 text-center mt-10">Human in the Loop<br/>AI Calling</div>
+      <div className="flex flex-row justify-center gap-10 mt-16">
+        <div className="flex flex-row items-center justify-center gap-4 mt-10">
+          <div>
+            <h1>Supervisor</h1>
+          </div>
+          <div className={twMerge("relative w-auto h-auto flex items-center justify-center")}>
+            <motion.div
+              className="absolute top-0 left-0 w-full h-full bg-blue-500 rounded-full opacity-30"
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.1, ease: 'easeOut' }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+              className="absolute top-0 left-0 w-full h-full bg-green-500 rounded-full opacity-30"
+            />
+            <AudioWaveform
+              className="text-blue-500"
+              size={50}
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-row items-center justify-center gap-4 mt-10">
+          <div>
+            <h1>Livekit</h1>
+          </div>
+          <div className={twMerge("relative w-auto h-auto flex items-center justify-center")}>
+            <motion.div
+              className="absolute top-0 left-0 w-full h-full bg-blue-500 rounded-full opacity-30"
+              animate={{ scale: 1}}
+              transition={{ duration: 0.1, ease: 'easeOut' }}
+            />
+            <div
+              className="absolute top-0 left-0 w-full h-full bg-green-500 rounded-full opacity-30"
+            />
+            <Bot
+              className="text-blue-500 p-1"
+              size={50}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
